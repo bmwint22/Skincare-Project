2e937bdc2966d4f76f430c5d3371236e761e56c0
Benita <bmwint22@gmail.com>
2025-02-02 19:40:42 -0600

    message describing code changes

--- models/user.js
+++ models/user.js
@@ -19,31 +19,31 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true
   },
- 
+
 });
 
-  
-  const userSchema = new mongoose.Schema({
-  
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-        type: String,
-    },
 
-    products: [productSchema], 
-  });
-  
+const userSchema = new mongoose.Schema({
+
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+  },
+
+  products: [productSchema],
+});
+
 
 
 const User = mongoose.model('User', userSchema)
-  module.exports = User
+module.exports = User
